d6ecd9bb551ce4812e705ce395364ba593d7e86c
ttran1017 <tylertran1017@gmail.com>
2025-11-09 22:37:06 -0700

    Add word list and build user loop
    Adds an unfinalized word list and handles user inputs. This is
    a functional prototype.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main()
+#include "words.h"
+
+#define BUFFER_SIZE 256
+
+uint8_t rand_digits(){
+    return rand() % 100;
+}
+
+int32_t main()
 {
-    printf("Hello world!\n");
+    srand(time(NULL));
+
+    char input[BUFFER_SIZE];
+    uint8_t rand_number;
+    for(;;){
+        rand_number = rand_digits();
+        printf("Random digits: %02d\n", rand_number);
+        printf("Input:");
+        if(fgets(input, BUFFER_SIZE, stdin) == NULL){
+            break;
+        }
+        printf("Word: %s | You've entered: %s\n", mnemonic_words[rand_number], input);
+
+    }
     return 0;
 }
