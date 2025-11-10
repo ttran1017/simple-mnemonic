4581c1deb467de6e07fb43a19f63d34c681e2a0a
ttran1017 <tylertran1017@gmail.com>
2025-11-09 23:57:41 -0700

    Implement scoring and buffer handling
    Implement safe buffer handling with fgets and clearing the buffer
    after an input. Score keeping is added.

--- src/main.c
+++ src/main.c
@@ -2,30 +2,54 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 #include "words.h"
 
-#define BUFFER_SIZE 256
+#define BUFFER_SIZE 10
 
-uint8_t rand_digits(){
+uint8_t rand_digits()
+{
     return rand() % 100;
+}
+
+void flush()
+{
+    char ch;
+    while ((ch = getchar()) != '\n' && ch != EOF)
+        ;
 }
 
 int32_t main()
 {
     srand(time(NULL));
 
-    char input[BUFFER_SIZE];
+    char buffer[BUFFER_SIZE];
+    const char *reference_word;
     uint8_t rand_number;
-    for(;;){
-        rand_number = rand_digits();
-        printf("Random digits: %02d\n", rand_number);
-        printf("Input:");
-        if(fgets(input, BUFFER_SIZE, stdin) == NULL){
-            break;
-        }
-        printf("Word: %s | You've entered: %s\n", mnemonic_words[rand_number], input);
+    uint32_t score = 0;
 
+    for (;;)
+    {
+        rand_number = rand_digits();
+        reference_word = mnemonic_words[rand_number];
+
+        printf("Current Score: %03d | Random digits: %02d\n", score, rand_number);
+        printf("Input:");
+        if (fgets(buffer, BUFFER_SIZE, stdin) != NULL)
+        {
+            size_t len = strlen(buffer);
+            if (len > 0 && buffer[len - 1] == '\n')
+            {
+                buffer[len - 1] = '\0';
+            }
+            else
+            {
+                flush();
+            }
+        }
+        printf("Reference word: %s | You've entered: %s\n", reference_word, buffer);
+        score = strcmp(buffer, reference_word) ? 0 : score + 1;
     }
     return 0;
 }
